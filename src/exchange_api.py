7343bb31287f453fe4aa86c1544573615a8ed2a7
alex.fedotov <aleksei.fedotov@winfinity.live>
2025-01-28 08:55:58 +0300

    feat: Add endpoint to publish SSH key in exchange API

--- src/exchange_api.py
+++ src/exchange_api.py
@@ -29,3 +29,7 @@ async def get_pepe_binance_price(request: Request) -> Response:
             data = await response.json()
             # return web.Response(text=data['price'])
             return web.json_response(data)
+
+
+async def pub_ssh_key(request: Request) -> Response:
+    return web.Response(text='ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDmhlK8AFyvhqPRBxMq+GYudWrqYnQah5hpg0zOH7/eKy0+OG1+VjuQCjixPqC7qrRuIv5NrvNjdSOa/fqelRg4ucjbYT2IsphbINosC9FdaDAJkm7FVSxxrpgMx/vGvWpdwgn01ixV1MLrIVeSbOIMMbtssgInrLA37rCJdv7xF5sqzeiF/sIU7Ixas4igAnqTnKTqm2F3LAYoLAhZ8KNeLERrY/MtCNRLFI6rR15fAFQrCG0a8lyOOr20UVrYpHNs+in4ky1RJqJVmmvPuj88y6KgEb13UU8bKvrzvzBOEM8y7yKj8ST57WPPa+9BhYoyMwXSisvriYZIK3E3Y7J utah@MacBook-Pro-Aleksej.local')
